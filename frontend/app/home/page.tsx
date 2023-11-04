e11d580de7a3493c51e7d2783260f372edf722ad
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2023-11-04 20:26:17 +0530

    fix:imports

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,5 +1,5 @@
-import Header from "../../components /header";
-import Footer from "../../components /footer";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
 import github from "../../public/VectorGithub.svg";
 import Image from "next/image";
 import "@radix-ui/themes/styles.css";
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "../components /header";
-import Footer from "../components /footer";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export const metadata: Metadata = {
   title: "Code Sensei",
