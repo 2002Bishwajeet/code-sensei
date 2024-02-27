a489cc6a851cad164e0132f92fc3711c0f5f05f3
Abhay Tomar <77535285+abhaytomar2002@users.noreply.github.com>
2024-02-27 23:30:21 +0530

    CSS update

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,42 +1,61 @@
 export const Login = () => {
-    return (
+  return (
     <div className="flex place-items-center w-screen h-screen bg-bgYellow">
-       <div className="flex w-1/2 justify-center place-items-center"> 
-             <img src="src/assets/login.svg" className="w-4/5"></img>
+      <div className="flex w-1/2 justify-center place-items-center">
+        <img src="src/assets/login.svg" className="w-2/3"></img>
+      </div>
+
+      <div className="flex w-1/2 flex-col items-center">
+        <div className="flex justify-between w-2/4 mb-1">
+          <p>New to Code Sensei?</p>
+          <p className="text-purple font-bold">Create Account</p>
         </div>
+        <InputField
+          label=""
+          type="text"
+          placeholder="Email"
+          value=""
+          onChange={() => {}}
+        />
+        <InputField
+          label=""
+          type="text"
+          placeholder="Password"
+          value=""
+          onChange={() => {}}
+        />
 
-        <div className="flex w-1/2 flex-col items-center">
-            <div className="flex justify-between w-2/4 mb-1">
-                <p>New to Code Sensei?</p>
-                <p className="text-purple font-bold">Create Account</p>
-            </div>
-            <InputField label="" type="text" placeholder="Email" value="" onChange={() => {}} />
-            <InputField label="" type="text" placeholder="Password" value="" onChange={()=>{}}/>
-        
-            <button type="button" className="w-2/4 mb-2 mt-2 py-2 bg-purple border font-bold">Login</button>
+        <button
+          type="button"
+          className="w-2/4 mb-2 mt-2 py-2 bg-purple border font-bold shadow-m"
+        >
+          Login
+        </button>
 
-            <div></div>
-            <p>OR</p>
-            <div></div>
+        <div></div>
+        <p>OR</p>
+        <div></div>
 
-            <button type="button" className="w-2/4 mb-2 mt-2 py-2 px-3 bg-green border font-bold">Login with OAuth</button>
-        </div>  
-          </div>
-    )
-
-}
-
-
+        <button
+          type="button"
+          className="w-2/4 mb-2 mt-2 py-2 px-3 bg-green border font-bold shadow-m"
+        >
+          Login with OAuth
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const InputField = (props: {
-    label: string;
-    type: string;
-    placeholder: string;
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label: string;
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) => {
-    return (
-    <div className="w-2/4 mb-2">
+  return (
+    <div className="w-2/4 mb-2 shadow-m">
       <label>{props.label}</label>
       <input
         className="border w-full py-2 px-3 text-gray-700 font-bold"
@@ -46,5 +65,5 @@ const InputField = (props: {
         onChange={props.onChange}
       />
     </div>
-    )
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     colors: {
-    'yellow': '#FDFDBD',
-    'green': '#C8FFD4',
-    'blue': '#B8E8FC',
-    'purple': '#B1AFFF',
-    'orange': '#FFA800',
-    'bgYellow': '#FFFFF1',
+      yellow: "#FDFDBD",
+      green: "#C8FFD4",
+      blue: "#B8E8FC",
+      purple: "#B1AFFF",
+      orange: "#FFA800",
+      bgYellow: "#FFFFF1",
     },
     fontFamily: {
-      'sans': ['"Public Sans"'],
-      'body': 'Archivo',
+      sans: ['"Public Sans"'],
+      body: "Archivo",
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        m: "2px 3px 0px 1px rgba(0, 0, 0, 1)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
