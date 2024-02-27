a581aa3f4314071e055392b64b02e594939361c7
dishukataria <dishukataria1212@gmail.com>
2024-02-27 23:40:36 +0530

    Added Icons

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -5,60 +5,44 @@ export const Login = () => {
         <img src="src/assets/login.svg" className="w-2/3"></img>
       </div>
 
-      <div className="flex w-1/2 flex-col items-center">
-        <div className="flex justify-between w-2/4 mb-1">
-          <p>New to Code Sensei?</p>
-          <p className="text-purple font-bold">Create Account</p>
-        </div>
-        <InputField
-          label=""
-          type="text"
-          placeholder="Email"
-          value=""
-          onChange={() => {}}
-        />
-        <InputField
-          label=""
-          type="text"
-          placeholder="Password"
-          value=""
-          onChange={() => {}}
-        />
+     
 
-        <button
-          type="button"
-          className="w-2/4 mb-2 mt-2 py-2 bg-purple border font-bold shadow-m"
-        >
-          Login
-        </button>
+        <div className="flex w-1/2 flex-col items-center">
+            <div className="flex justify-between w-2/4 mb-1">
+                <p>New to Code Sensei?</p>
+                <p className="text-purple font-bold">Create Account</p>
+            </div>
+            <InputField label="" type="text" placeholder="Email" onChange={() => {}} imageSrc="src/assets/mailIcon.svg"/>
+            <InputField label="" type="text" placeholder="Password" onChange={()=>{}} imageSrc="src/assets/lockIcon.svg"/>
+        
+            <button type="button" className="w-2/4 mb-2 mt-2 py-2 bg-purple border font-bold shadow-m">Login</button>
 
-        <div></div>
-        <p>OR</p>
-        <div></div>
+            <div className="flex">
+            <div className='border-solid border-b-8'></div>
+            <p className="font-bold">OR</p>
+            <div></div>
+            </div>
 
-        <button
-          type="button"
-          className="w-2/4 mb-2 mt-2 py-2 px-3 bg-green border font-bold shadow-m"
-        >
-          Login with OAuth
-        </button>
-      </div>
-    </div>
+            <button type="button" className="w-2/4 mb-2 mt-2 py-2 px-3 bg-green border font-bold shadow-m">Login with OAuth</button>
+        </div>  
+          </div>
+    
   );
 };
 
 const InputField = (props: {
-  label: string;
-  type: string;
-  placeholder: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    label: string;
+    type: string;
+    placeholder: string;
+    value?: string;
+    imageSrc: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) => {
-  return (
-    <div className="w-2/4 mb-2 shadow-m">
-      <label>{props.label}</label>
+    return (
+    <div className="flex flex-row border w-2/4 mb-2 py-2 px-3 text-gray-700 font-bold bg-white shadow-m">
+      <img src={props.imageSrc} className="mr-2 w-6 h-7" alt={props.label}/>
       <input
-        className="border w-full py-2 px-3 text-gray-700 font-bold"
+        className="w-5/6"
         type={props.type}
         placeholder={props.placeholder}
         value={props.value}
