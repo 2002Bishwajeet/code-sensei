1baf13413502d457ab5f3cdbe5bb71b55997eae4
Abhay Tomar <77535285+abhaytomar2002@users.noreply.github.com>
2024-02-29 01:25:14 +0530

    Css updates

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,41 +1,85 @@
 export const Login = () => {
   return (
-    <div className="flex place-items-center w-screen h-screen bg-bgYellow">
-      <div className="flex w-1/2 justify-center place-items-center">
-        <img src="src/assets/login.svg" className="w-2/3"></img>
-      </div>
+    <div className="w-screen h-screen bg-bgYellow">
+      <img
+        className="h-28 absolute top-8 left-16"
+        src="src/assets/illustration_topleft_login.svg"
+        alt="image"
+      />
+      <img
+        className="h-52 absolute object-right-top -right-28"
+        src="src/assets/illustration_topright_login.svg"
+        alt="image"
+      />
+      <img
+        className="h-40 absolute bottom-0 right-0"
+        src="src/assets/Illustration_rightcorner_login.svg"
+        alt="image"
+      />
+      <img
+        className="h-16 absolute bottom-10 left-1/3"
+        src="src/assets/Illustration_bottom_login.svg"
+        alt="image"
+      />
+      <div className="flex place-items-center h-screen w-screen">
+        <div className="flex w-1/2 justify-center place-items-center">
+          <img src="src/assets/login.svg" className="w-2/3"></img>
+        </div>
         <div className="flex w-1/2 flex-col items-center">
-            <div className="flex justify-between w-2/4 mb-1">
-                <p>New to Code Sensei?</p>
-                <p className="text-purple font-bold">Create Account</p>
-            </div>
-            <InputField label="" type="text" placeholder="Email" onChange={() => {}} imageSrc="src/assets/mailIcon.svg"/>
-            <InputField label="" type="text" placeholder="Password" onChange={()=>{}} imageSrc="src/assets/lockIcon.svg"/>
-        
-            <button type="button" className="w-2/4 mb-2 mt-2 py-2 bg-purple border font-bold shadow-m">Login</button>
+          <div className="flex justify-between w-2/4 mb-1">
+            <p>New to Code Sensei?</p>
+            <p className="text-purple font-bold">Create Account</p>
+          </div>
+          <InputField
+            label=""
+            type="text"
+            placeholder="Email"
+            onChange={() => {}}
+            imageSrc="src/assets/mailIcon.svg"
+          />
+          <InputField
+            label=""
+            type="text"
+            placeholder="Password"
+            onChange={() => {}}
+            imageSrc="src/assets/lockIcon.svg"
+          />
 
-            <div className="flex">
-              <div></div>
-              <p className="font-bold">OR</p>
-              <div></div>
-            </div>
-            <button type="button" className="w-2/4 mb-2 mt-2 py-2 px-3 bg-green border font-bold shadow-m">Login with OAuth</button>
-        </div>  
+          <button
+            type="button"
+            className="w-2/4 mb-2 mt-2 py-2 bg-purple border font-bold shadow-m"
+          >
+            Login
+          </button>
+
+          <div className="flex place-items-center">
+            <div className="border w-36 h-0"></div>
+            <p className="font-bold px-4">OR</p>
+            <div className="border w-36 h-0"></div>
+          </div>
+          <button
+            type="button"
+            className="w-2/4 mb-2 mt-2 py-2 px-3 bg-green border font-bold shadow-m"
+          >
+            Login with OAuth
+          </button>
+        </div>
       </div>
+    </div>
   );
 };
 
 const InputField = (props: {
-    label: string;
-    type: string;
-    placeholder: string;
-    value?: string;
-    imageSrc: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label: string;
+  type: string;
+  placeholder: string;
+  value?: string;
+  imageSrc: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }) => {
-    return (
+  return (
     <div className="flex flex-row border w-2/4 mb-2 py-2 px-3 text-gray-700 font-bold bg-white shadow-m">
-      <img src={props.imageSrc} className="mr-2 w-6 h-7" alt={props.label}/>
+      <img src={props.imageSrc} className="mr-2 w-6 h-7" alt={props.label} />
       <input
         className="w-5/6"
         type={props.type}
