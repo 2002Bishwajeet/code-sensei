098f388e1819c06770557e72fe213b38062426df
Bishwajeet Parhi <bishwajeet.techmaster@gmail.com>
2024-04-18 21:16:29 +0530

    commit

--- src/routes/Chat.tsx
+++ src/routes/Chat.tsx
@@ -1,7 +1,45 @@
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Chat = () => {
-  const navigation = useNavigate();
+  // useEffect(() => {
+  //   // fetch(new URL('https://api.runpod.ai/v2/mpnlge4vkgddpo/runsync'),)
+
+  //   return () => {
+  //     second
+  //   }
+  // }, [third])
+  const [loading, setLoading] = useState(false);
+
+  const sendChat = async (input: string) => {
+    console.log("Sencd Chat called");
+    setLoading(true);
+    const response = await fetch(
+      new URL("https://api.runpod.ai/v2/mpnlge4vkgddpo/runsync"),
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: import.meta.env.VITE_RUNPODKEY,
+        },
+        body: JSON.stringify({
+          input: {
+            prompt: input,
+            sampling_params: {
+              max_tokens: 1000,
+              n: 1,
+              presence_penalty: 0.2,
+              frequency_penalty: 0.7,
+              temperature: 0.3,
+            },
+          },
+        }),
+      }
+    );
+    setLoading(false);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="w-screen h-screen bg-bgYellow">
       <img
@@ -30,8 +68,14 @@ export const Chat = () => {
           />
         </div>
       </div>
+
       {/* the chat send box */}
       <div className="flex flex-col relative h-4/5 ">
+        {loading && (
+          <div className="flex justify-center items-center h-4/5">
+            <p> Loading</p>
+          </div>
+        )}
         <div className="flex justify-center absolute inset-x-0 bottom-0">
           <form className="flex items-center w-1/2 border px-3 py-2 text-gray-700 font-bold bg-white shadow-m">
             <input
@@ -43,6 +87,16 @@ export const Chat = () => {
             <button
               type="submit"
               className="pl-3 bg-green border font-bold shadow-m"
+              onClick={(e) => {
+                e.preventDefault();
+                const input = (
+                  document.getElementById("text") as HTMLInputElement
+                ).value;
+                if (input === "") return;
+                sendChat(input);
+                (document.getElementById("text") as HTMLInputElement).value =
+                  "";
+              }}
             >
               <img
                 src="src/assets/Send_icon.svg"
